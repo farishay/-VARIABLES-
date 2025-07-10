28bb548c608f877796963008797794cbbb8a218a
Farwa Tariq <farwa.tariq2434@gmail.com>
2025-07-10 16:39:36 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+
+// js assignment 2 "VARIABLES"
+
+// 1. Declare a variable called username.
+let username; 
+
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+
+let myName = "Ferishay Hayat"; 
+console.log(myName);
+
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+ let message;
+message = "Hello World 🌎"; 
+alert( "ans : 3 \n" + message); 
+
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+let studentName = "Ferishayy Hayat";
+let age = 21;
+let course = "Web Development";
+alert(  "ans : 4" + " \n Name: " + studentName + "\n Age: " + age + "\n Course: " + course);
+
+// 5. Write a script to display the following alert using one JS
+// variable:
+
+let carpediem = "CARPE DIEM \n CARPE DIE \n CARPE DI \n CARPE D \n CARPE \n CARP \n CAR \n CA \n C";
+alert("ans : 5 \n" + carpediem);
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+let email = "farwa.2434tariq@gmail.com";
+alert("ans : 6 \n My email address is : " + email );
+
+// 7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”.
+let book = "A smarter way to learn JavaScript";
+alert("ans : 7 \n" + "I am trying to learn from the Book " + book);
+
+// 8. Write a script to display this in browser through JS
+document.writeln("ans : 8 <br> " + "Yah! I can write HTML content through JavaScript <br>");
+
+// 9. Store following string in a variable and show in alert and browser through JS
+let css = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬";
+alert("ans : 9 \n" + css);
+document.writeln("<br> ans : 9 <br> " + css);
+ 
